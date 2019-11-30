7ae7de051c4c67b447a1c0423d6b4437f53675c9
keshupooh <752342092@qq.com>
2019-11-30 14:36:52 +0800

    Initial commit

---
+++ Example/DSTBookKit/DSTViewController.h
@@ -0,0 +1,13 @@
+//
+//  DSTViewController.h
+//  DSTBookKit
+//
+//  Created by keshupooh on 11/30/2019.
+//  Copyright (c) 2019 keshupooh. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DSTViewController : UIViewController
+
+@end
---
+++ Example/DSTBookKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DSTBookKit
+//
+//  Created by keshupooh on 11/30/2019.
+//  Copyright (c) 2019 keshupooh. All rights reserved.
+//
+
+@import UIKit;
+#import "DSTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DSTAppDelegate class]));
+    }
+}
